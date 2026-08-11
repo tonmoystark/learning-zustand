a2e6b1dc1f23fd10bfdd77a564aa83435e7d84d3
tonmoystark <tonmoy.a009@gmail.com>
2026-08-11 22:30:37 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-gray-900 text-white">
+      <h1 className="text-2xl text-center font-bold py-5">Learning Zustand</h1>
+    </div>
+  )
+}
+
+export default App
