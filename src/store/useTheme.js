bfc7c48254deab04c010d8474d8128e4e896b6e5
tonmoystark <tonmoy.a009@gmail.com>
2026-08-13 22:36:13 +0600

    learning persist

--- src/store/useTheme.js
+++ src/store/useTheme.js
@@ -1,13 +1,26 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useTheme = create((set) => ({
-  theme: "light",
+// export const useTheme = create((set) => ({
+//   theme: "light",
 
-  handleTheme: () =>
-    set((state) => ({
-      theme:
-        state.theme === "light"
-          ? (state.theme = "dark")
-          : (state.theme = "light"),
-    })),
-}));
+//   handleTheme: () =>
+//     set((state) => ({
+//       theme:
+//         state.theme === "light"
+//           ? (state.theme = "dark")
+//           : (state.theme = "light"),
+//     })),
+// }));
+
+//learning persist
+
+export const useTheme = create(
+    persist((set) => ({
+        theme: "light",
+
+        handleTheme: () => set(state => ({
+            theme: state.theme === "light" ? "dark" : "light"
+        }))
+    }))
+)
